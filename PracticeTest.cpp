8bb1727735875a35c08fef7c091bdace56abdba0
lcnorine <60401135+lcnorine@users.noreply.github.com>
2020-04-22 16:36:16 -0700

    added comprehensive tests

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -14,25 +14,60 @@ class PracticeTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-TEST(PracticeTest, is_even_palindrome)
+TEST(PracticeTest, is_short_even_palindrome)
 {
     Practice obj;
     bool actual = obj.isPalindrome("aa");
     ASSERT_TRUE(actual);
 }
 
-TEST(PracticeTest, is_not_a_palindrome)
+TEST(PracticeTest, is_short_even_not_palindrome)
 {
     Practice obj;
-    bool actual = obj.isPalindrome("Ba");
+    bool actual = obj.isPalindrome("za");
     ASSERT_FALSE(actual);
 }
 
-TEST(PracticeTest, is_odd_palindrome)
+TEST(PracticeTest, is_long_even_palindrome)
 {
     Practice obj;
-    bool actual = obj.isPalindrome("BaB");
+    bool actual = obj.isPalindrome("azza");
     ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_long_even_not_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("zaza");
+    ASSERT_FALSE(actual);
+}
+
+TEST(PracticeTest, is_short_odd_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("zaz");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_short_odd_not_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("zaa");
+    ASSERT_FALSE(actual);
+}
+
+TEST(PracticeTest, is_long_odd_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("zazaz");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_long_odd_not_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("zaazz");
+    ASSERT_FALSE(actual);
 }
 
 TEST(PracticeTest, is_single_letter_palindrome)
@@ -42,13 +77,165 @@ TEST(PracticeTest, is_single_letter_palindrome)
     ASSERT_TRUE(actual);
 }
 
+TEST(PracticeTest, is_nothing_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("");
+    ASSERT_TRUE(actual);
+}
+
 TEST(PracticeTest, is_ascending)
 {
     Practice obj;
-	int i1 = 1;
-	int i2 = 2;
-	int i3 = 3;
+	int i1 = -1;
+	int i2 = 0;
+	int i3 = 1;
     obj.sortDescending(i1,i2,i3);
-    ASSERT_EQ(i1,3);
-	ASSERT_EQ(i2,2);
+    ASSERT_EQ(i1,1);
+	ASSERT_EQ(i2,0);
+	ASSERT_EQ(i3,-1);
+}
+
+TEST(PracticeTest, is_descending)
+{
+    Practice obj;
+	int i1 = 1;
+	int i2 = 0;
+	int i3 = -1;
+    obj.sortDescending(i1,i2,i3);
+    ASSERT_EQ(i1,1);
+	ASSERT_EQ(i2,0);
+	ASSERT_EQ(i3,-1);
+}
+
+TEST(PracticeTest, is_first_mixed_ascending)
+{
+    Practice obj;
+	int i1 = 0;
+	int i2 = -1;
+	int i3 = 1;
+    obj.sortDescending(i1,i2,i3);
+    ASSERT_EQ(i1,1);
+	ASSERT_EQ(i2,0);
+	ASSERT_EQ(i3,-1);
+}
+
+TEST(PracticeTest, is_first_mixed_descending)
+{
+    Practice obj;
+	int i1 = 0;
+	int i2 = 1;
+	int i3 = -1;
+    obj.sortDescending(i1,i2,i3);
+    ASSERT_EQ(i1,1);
+	ASSERT_EQ(i2,0);
+	ASSERT_EQ(i3,-1);
+}
+
+TEST(PracticeTest, is_last_mixed_ascending)
+{
+    Practice obj;
+	int i1 = -1;
+	int i2 = 1;
+	int i3 = 0;
+    obj.sortDescending(i1,i2,i3);
+    ASSERT_EQ(i1,1);
+	ASSERT_EQ(i2,0);
+	ASSERT_EQ(i3,-1);
+}
+
+TEST(PracticeTest, is_last_mixed_descending)
+{
+    Practice obj;
+	int i1 = 1;
+	int i2 = -1;
+	int i3 = 0;
+    obj.sortDescending(i1,i2,i3);
+    ASSERT_EQ(i1,1);
+	ASSERT_EQ(i2,0);
+	ASSERT_EQ(i3,-1);
+}
+
+TEST(PracticeTest, is_one_smaller_first)
+{
+    Practice obj;
+	int i1 = -1;
+	int i2 = 1;
+	int i3 = 1;
+    obj.sortDescending(i1,i2,i3);
+    ASSERT_EQ(i1,1);
+	ASSERT_EQ(i2,1);
+	ASSERT_EQ(i3,-1);
+}
+
+TEST(PracticeTest, is_one_smaller_middle)
+{
+    Practice obj;
+	int i1 = 1;
+	int i2 = -1;
+	int i3 = 1;
+    obj.sortDescending(i1,i2,i3);
+    ASSERT_EQ(i1,1);
+	ASSERT_EQ(i2,1);
+	ASSERT_EQ(i3,-1);
+}
+
+TEST(PracticeTest, is_one_smaller_last)
+{
+    Practice obj;
+	int i1 = 1;
+	int i2 = 1;
+	int i3 = -1;
+    obj.sortDescending(i1,i2,i3);
+    ASSERT_EQ(i1,1);
+	ASSERT_EQ(i2,1);
+	ASSERT_EQ(i3,-1);
+}
+
+TEST(PracticeTest, is_one_larger_first)
+{
+    Practice obj;
+	int i1 = 1;
+	int i2 = -1;
+	int i3 = -1;
+    obj.sortDescending(i1,i2,i3);
+    ASSERT_EQ(i1,1);
+	ASSERT_EQ(i2,-1);
+	ASSERT_EQ(i3,-1);
+}
+
+TEST(PracticeTest, is_one_larger_middle)
+{
+    Practice obj;
+	int i1 = -1;
+	int i2 = 1;
+	int i3 = -1;
+    obj.sortDescending(i1,i2,i3);
+    ASSERT_EQ(i1,1);
+	ASSERT_EQ(i2,-1);
+	ASSERT_EQ(i3,-1);
+}
+
+TEST(PracticeTest, is_one_larger_last)
+{
+    Practice obj;
+	int i1 = -1;
+	int i2 = -1;
+	int i3 = 1;
+    obj.sortDescending(i1,i2,i3);
+    ASSERT_EQ(i1,1);
+	ASSERT_EQ(i2,-1);
+	ASSERT_EQ(i3,-1);
+}
+
+TEST(PracticeTest, is_all_same)
+{
+    Practice obj;
+	int i1 = 0;
+	int i2 = 0;
+	int i3 = 0;
+    obj.sortDescending(i1,i2,i3);
+    ASSERT_EQ(i1,0);
+	ASSERT_EQ(i2,0);
+	ASSERT_EQ(i3,0);
 }
